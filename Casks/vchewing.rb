d3ed56ee700559f240519031d74b9c20c3a41c97
SiongSng <SiongSng@users.noreply.github.com>
2026-07-23 14:28:50 +0800

    feat: add vchewing cask using GitHub release source

---
+++ Casks/vchewing.rb
@@ -0,0 +1,29 @@
+cask "vchewing" do
+  version "4.5.5"
+  sha256 "a114563decce62759d7ef03288b42b1ae524791fd8af154daf5b1144ba5cab3e"
+
+  url "https://github.com/vChewing/vChewing-macOS/releases/download/#{version}/vChewing-macOS-#{version}-signed.pkg"
+  name "vChewing (唯音輸入法)"
+  desc "[macOS 最安全的副廠注音輸入法] Phonabet-based input methods for typing Mandarin Chinese, providing native support for typing both Simplified and Traditional Chinese without conversions between them. This might be the only Chinese input method (for macOS) which is Sandboxed, and it won't send your data to anywhere outside your computer (nor security vulnerabilities)."
+  homepage "https://vchewing.github.io/"
+
+  pkg "vChewing-macOS-#{version}-signed.pkg"
+
+  postflight do
+    ohai "==============================="
+    ohai "請務必考慮贊助敝專案的開發。詳情：https://vchewing.github.io/"
+    ohai ""
+    ohai "Donations are warmly welcomed. More info at homepage. https://vchewing.github.io/"
+    ohai "==============================="
+  end
+
+  uninstall delete: "~/Library/Input\ Methods/vChewing.app"
+  uninstall delete: "~/Library/Keyboard\ Layouts/vChewingKeyLayout.bundle"
+  uninstall delete: "~/Library/Keyboard\ Layouts/vChewing\ MiTAC.keylayout"
+  uninstall delete: "~/Library/Keyboard\ Layouts/vChewing\ IBM.keylayout"
+  uninstall delete: "~/Library/Keyboard\ Layouts/vChewing\ FakeSeigyou.keylayout"
+  uninstall delete: "~/Library/Keyboard\ Layouts/vChewing\ ETen.keylayout"
+  uninstall delete: "~/Library/Keyboard\ Layouts/vChewing\ Dachen.keylayout"
+  uninstall delete: "~/Library/Receipts/org.atelierInmu.vChewing.bom"
+  uninstall delete: "~/Library/Receipts/org.atelierInmu.vChewing.plist"
+end
