b0253e63f3638604ee6fa722f1aca4cca52e0901
菘菘 <tcabbagespace@gmail.com>
2026-09-08 11:11:43 +0800

    fix: replace deprecated postflight with caveats stanza

--- Casks/vchewing.rb
+++ Casks/vchewing.rb
@@ -9,21 +9,23 @@ cask "vchewing" do
 
   pkg "vChewing-macOS-#{version}-signed.pkg"
 
-  postflight do
-    ohai "==============================="
-    ohai "請務必考慮贊助敝專案的開發。詳情：https://vchewing.github.io/"
-    ohai ""
-    ohai "Donations are warmly welcomed. More info at homepage. https://vchewing.github.io/"
-    ohai "==============================="
-  end
+  uninstall delete: [
+    "~/Library/Input Methods/vChewing.app",
+    "~/Library/Keyboard Layouts/vChewing Dachen.keylayout",
+    "~/Library/Keyboard Layouts/vChewing ETen.keylayout",
+    "~/Library/Keyboard Layouts/vChewing FakeSeigyou.keylayout",
+    "~/Library/Keyboard Layouts/vChewing IBM.keylayout",
+    "~/Library/Keyboard Layouts/vChewing MiTAC.keylayout",
+    "~/Library/Keyboard Layouts/vChewingKeyLayout.bundle",
+    "~/Library/Receipts/org.atelierInmu.vChewing.bom",
+    "~/Library/Receipts/org.atelierInmu.vChewing.plist",
+  ]
 
-  uninstall delete: "~/Library/Input\ Methods/vChewing.app"
-  uninstall delete: "~/Library/Keyboard\ Layouts/vChewingKeyLayout.bundle"
-  uninstall delete: "~/Library/Keyboard\ Layouts/vChewing\ MiTAC.keylayout"
-  uninstall delete: "~/Library/Keyboard\ Layouts/vChewing\ IBM.keylayout"
-  uninstall delete: "~/Library/Keyboard\ Layouts/vChewing\ FakeSeigyou.keylayout"
-  uninstall delete: "~/Library/Keyboard\ Layouts/vChewing\ ETen.keylayout"
-  uninstall delete: "~/Library/Keyboard\ Layouts/vChewing\ Dachen.keylayout"
-  uninstall delete: "~/Library/Receipts/org.atelierInmu.vChewing.bom"
-  uninstall delete: "~/Library/Receipts/org.atelierInmu.vChewing.plist"
+  caveats <<~EOS
+    請至「系統設定 → 鍵盤 → 輸入來源」啟用 vChewing。
+    請務必考慮贊助敝專案的開發。詳情：https://vchewing.github.io/
+
+    Enable vChewing in System Settings → Keyboard → Input Sources.
+    Donations are warmly welcomed. More info at https://vchewing.github.io/
+  EOS
 end
